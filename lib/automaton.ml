26977ae964154a5c5d4b70ba4ba6c25dd502aec6
Robotechnic <robotechnic.developeur@gmail.com>
2023-03-08 00:10:42 +0100

    add line track to the automaton to have better error display

--- lib/automaton.ml
+++ lib/automaton.ml
@@ -2,53 +2,59 @@ module AutomatonEvent = Map.Make(String)
 
 type automaton = {
   state : string;
-  events : string AutomatonEvent.t AutomatonEvent.t
+  events : (int * string AutomatonEvent.t) AutomatonEvent.t
 }
 
 (**
   Check if all events defined in the automaton actions exists in the automaton
 *)
 let checkAutomatonEvents map =
-  let rec checkEvents = function
+  let rec checkEvents line = function
   | [] -> ()
-  | (event, _)::tails ->
-    if not (AutomatonEvent.mem event map) then
-      failwith ("Event " ^ event ^ " does not exist")
+  | (_, endNode)::tails ->
+    if not (AutomatonEvent.mem endNode map) then
+      let () = Printf.eprintf "Error: End node \"%s\" defined at line %d does not exist in automaton" endNode line in exit 1
     else
-      checkEvents tails
-    in AutomatonEvent.iter (fun _ events -> checkEvents (AutomatonEvent.bindings events)) map
+      checkEvents line tails
+    in AutomatonEvent.iter (fun _ (line, events) -> checkEvents line (AutomatonEvent.bindings events)) map
 
 let buildAutomaton baseState events =
   match baseState with
-  | Lexer.Action _ | Lexer.Test _ -> failwith "Base state must be a state"
-  | Lexer.Init (_, state) -> begin
+  | Lexer.Action _ | Lexer.Test _ | Lexer.Node _ -> failwith "Base state must be a state"
+  | Lexer.Init ((line, _), state) -> begin
     let rec buildEvents map = function
     | [] -> map
-    | Lexer.Action(_, from, if_, to_)::tails ->
+    | Lexer.Action(line, from, if_, to_)::tails ->
       if AutomatonEvent.mem from map then
         let events = AutomatonEvent.find from map in
-        buildEvents (AutomatonEvent.add from (AutomatonEvent.add if_ to_ events) map) tails
+        buildEvents (AutomatonEvent.add from (line, (AutomatonEvent.add if_ to_ (snd events))) map) tails
       else
-        buildEvents (AutomatonEvent.add from (AutomatonEvent.singleton if_ to_) map) tails
+        buildEvents (AutomatonEvent.add from (line, (AutomatonEvent.singleton if_ to_)) map) tails
+    | Lexer.Node(line, literal)::tails -> 
+      if AutomatonEvent.mem literal map then
+        buildEvents map tails
+      else
+        buildEvents (AutomatonEvent.add literal (line, AutomatonEvent.empty) map) tails
     | _ -> failwith "Lines must be actions"
     in let events = buildEvents AutomatonEvent.empty events 
     in let () = checkAutomatonEvents events
     in if AutomatonEvent.mem state events then
         {state = state; events = events}
       else
-        failwith "Base state does not exist"
+        let () = Printf.eprintf "Error: Base state \"%s\" defined at line %d does not exist in automaton" state line in exit 1
   end
 
 let rec runAutomaton automaton = function
 | [] -> automaton.state
 | event::tail ->
-  let events = AutomatonEvent.find automaton.state automaton.events in
+  let _, events = AutomatonEvent.find automaton.state automaton.events in
   let state = AutomatonEvent.find_opt event events in
-  if state = None then failwith ("Event " ^ event ^ " is not applicable from state " ^ automaton.state)
+  if state = None then 
+    failwith "Check failed in automaton builder"
   else runAutomaton {automaton with state = Option.get state} tail
 
 let runTest automaton = function
-| Lexer.Action _ | Lexer.Init _ -> failwith "Test must be a test"
+| Lexer.Action _ | Lexer.Init _ | Lexer.Node _ -> failwith "Test must be a test"
 | Lexer.Test (line, events, expected) ->
   let result = runAutomaton automaton events in
   result = expected, line
